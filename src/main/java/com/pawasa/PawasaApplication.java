8daf87b4ea9ccd876688db0642c3afa68c4e6e60
dep try <cdung288@gmail.com>
2022-09-25 20:34:15 +0700

    init project

---
+++ src/main/java/com/pawasa/PawasaApplication.java
@@ -0,0 +1,13 @@
+package com.pawasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawasaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawasaApplication.class, args);
+    }
+
+}
