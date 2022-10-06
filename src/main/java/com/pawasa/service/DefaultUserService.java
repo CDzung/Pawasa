90d60615af98129517554f9a2b390af6ccafe5db
dep try <cdung288@gmail.com>
2022-10-06 07:55:29 +0700

    add login

--- src/main/java/com/pawasa/service/DefaultUserService.java
+++ src/main/java/com/pawasa/service/DefaultUserService.java
@@ -3,6 +3,8 @@ package com.pawasa.service;
 import com.pawasa.exception.UserAlreadyExistsException;
 import com.pawasa.model.User;
 import com.pawasa.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,25 +15,17 @@ public class DefaultUserService implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     private final static String passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$";
 
-    private PasswordEncoder passwordEncoder = new PasswordEncoder() {
-        @Override
-        public String encode(CharSequence charSequence) {
-            return charSequence.toString();
-        }
-
-        @Override
-        public boolean matches(CharSequence charSequence, String s) {
-            return charSequence.toString().equals(s);
-        }
-    };
 
     @Override
     public void addUser(User user) throws UserAlreadyExistsException {
-        User existingUser = userRepository.findByUsernameOrEmail(user.getUsername(), user.getEmail());
+        User existingUser = userRepository.findByEmail(user.getEmail());
         if(existingUser != null) {
-            throw new UserAlreadyExistsException("User already exists with username: " + user.getUsername());
+            throw new UserAlreadyExistsException("User already exists with email: " + user.getEmail());
         }
         if(!user.getPassword().matches(passwordRegex)) {
             throw new IllegalArgumentException("Password must contain at least one digit, one lowercase, one uppercase, one special character and must be at least 8 characters long.");
@@ -43,4 +37,9 @@ public class DefaultUserService implements UserService {
     private void encodePassword(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
     }
+
+    public String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
 }
+
