a9c37b529f2cb18ddb69d6fda77ff91280c07597
kienbeo23071995 <KIENNHHE163054@fpt.edu.vn>
2022-10-09 13:34:29 +0700

    complete home

--- src/main/java/com/pawasa/Controller/client/ClientController.java
+++ src/main/java/com/pawasa/Controller/client/ClientController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClientController {
-    @GetMapping("home")
+    @GetMapping("/")
     public String getHomepage(){
         return "pages/client/home";
     }
