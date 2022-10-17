6ccaf4c6fed2da3ddd0b10f9bb516803f4974208
HaNgocHieu0301 <hieuhn0301@gmail.com>
2022-10-18 00:09:32 +0700

    Merge branch 'mido' of https://github.com/CDzung/Pawasa into hieu
     Conflicts:
    	src/main/java/com/pawasa/controller/client/BaseController.java
    	src/main/java/com/pawasa/controller/client/CustomerController.java
    	src/main/java/com/pawasa/controller/manager/ManagerController.java
    	src/main/java/com/pawasa/model/Order.java
    	src/main/java/com/pawasa/model/OrderDetail.java
    	src/main/java/com/pawasa/model/Product.java
    	src/main/java/com/pawasa/model/User.java
    	src/main/java/com/pawasa/service/CartService.java
    	src/main/java/com/pawasa/service/DefaultUserService.java
    	src/main/java/com/pawasa/service/ProductService.java
    	src/main/java/com/pawasa/service/UserService.java
    	src/main/resources/templates/pages/client/login.html
    	src/main/resources/templates/pages/manager/addproduct.html

--- src/main/java/com/pawasa/repository/ProductRepository.java
+++ src/main/java/com/pawasa/repository/ProductRepository.java
@@ -4,10 +4,8 @@ import com.pawasa.model.Product;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Set;
 
 @Repository
